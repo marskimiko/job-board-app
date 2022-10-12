8a3f0ed8522e277bca7e5cfed1a580ae1de6ce16
marisasaunders <marisaksaunders@gmail.com>
2022-10-12 17:53:59 -0400

    delete listing controller starter code

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,37 +1,26 @@
 class ListingsController < ApplicationController
 
-  # GET: /listings
+
   get "/listings" do
-    erb :"/listings/index.html"
+
   end
 
-  # GET: /listings/new
+
   get "/listings/new" do
-    erb :"/listings/new.html"
   end
 
-  # POST: /listings
   post "/listings" do
-    redirect "/listings"
   end
 
-  # GET: /listings/5
   get "/listings/:id" do
-    erb :"/listings/show.html"
   end
 
-  # GET: /listings/5/edit
   get "/listings/:id/edit" do
-    erb :"/listings/edit.html"
   end
 
-  # PATCH: /listings/5
   patch "/listings/:id" do
-    redirect "/listings/:id"
   end
 
-  # DELETE: /listings/5/delete
   delete "/listings/:id/delete" do
-    redirect "/listings"
   end
 end
