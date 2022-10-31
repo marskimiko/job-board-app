c0b033f9227aabc61b0ffcb6fb2b42e4b164b9bc
marisasaunders <marisaksaunders@gmail.com>
2022-10-31 16:56:56 -0400

    remove user from cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,8 @@
 class Cat < ActiveRecord::Base
   has_many :listings
-  # has_many :users, through: :listings
 end
+
+# class Cat < ActiveRecord::Base
+#   has_many :listings
+#   # has_many :users, through: :listings
+# end
