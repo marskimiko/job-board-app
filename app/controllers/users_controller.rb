ce8abe77108c03759470c6f03f5d27d1f96f26b1
marisasaunders <marisaksaunders@gmail.com>
2022-10-31 16:49:22 -0400

    clean user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 require './config/environment'
 
 class UserController < Sinatra::Base
-  # set :default_content_type, 'application/json'
 
   configure do
     set :default_content_type, 'application/json'
   end
-
-  # get "/" do
-  #   "Bye World"
-  # end
 
 end
